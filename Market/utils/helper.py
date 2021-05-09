94e80ad569d877c7d842e2b72e99b2c81cde897d
savimf <79181731+savimf@users.noreply.github.com>
2021-05-08 23:34:07 -0300

    Versão 2.1
    Docstring introduzida.

--- Market/utils/helper.py
+++ Market/utils/helper.py
@@ -1,2 +1,12 @@
 def coin_float_to_str(valor: float) -> str:
+    """Recebe um float e o retorna formatado como
+    string: R$ x,xxx.xx.
+
+    Args:
+        valor (float): valor a ser convertido, da
+        forma xxxx.xx.
+
+    Returns:
+        str: valor convertido, da forma 'R$ x,xxx.xx.'
+    """
     return f'R$ {valor:,.2f}'
