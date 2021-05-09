636a73c2f7dc649503be3760b1709c2c15bbcff3
savimf <79181731+savimf@users.noreply.github.com>
2021-05-08 23:33:25 -0300

    Versão 2.1
    Docstrings introduzidas.

--- Market/market.py
+++ Market/market.py
@@ -8,7 +8,7 @@ i) cadastrar produto;
 ii) listar produtos;
 iii) comprar produtos;
 iv) visualizar carrinho;
-v) sair da aplicação;
+v) sair da aplicação.
 
 Quando um produto já existente no carrinho for readicionado, sua quantidade
 somente será atualizada, tal que o mesmo não será duplicado.
@@ -21,15 +21,21 @@ from models.carrinho import Carrinho
 from utils.helper import coin_float_to_str
 from time import sleep
 
+# lista para armazenamento dos produtos
 produtos: list = []
+
+# objeto do tipo carrinho (ver classe Carrinho)
 carrinho: Carrinho = Carrinho()
 
 
 def main() -> None:
+    """Função principal, responsável pela execução do programa."""
     menu()
 
 
 def menu() -> None:
+    """Função responsável pela exibição do menu e execução das opções."""
+
     print('==================\n'
           '== Bem-vindo(a) ==\n'
           '==   Savi Shop  ==\n'
@@ -44,7 +50,7 @@ def menu() -> None:
           '6 - Fechar pedido\n'
           '7 - Sair')
 
-    opcao: int = int(input())
+    opcao: int = int(input('Opção: '))
 
     if opcao in range(1, 7):
         opcoes.get(opcao)()
@@ -59,13 +65,17 @@ def menu() -> None:
 
 
 def cadastrar_produto() -> None:
+    """Função responsável pelo cadastramento de um produto. Solicita ao
+    usuário as informações do mesmo, como nome e preço, e o adiciona à
+    lista 'produtos'."""
+
     print('Cadastro de produto\n'
           '===================')
 
-    nome: str = input('Nome do produto: ')
-    preco: float = float(input('Preço: '))
-
-    produto: Produto = Produto(nome, preco)
+    produto: Produto = Produto(
+        input('Nome do produto: '),
+        float(input('Preço: '))
+    )
 
     produtos.append(produto)
 
@@ -75,6 +85,9 @@ def cadastrar_produto() -> None:
 
 
 def listar_produtos() -> None:
+    """Função responsável pela listagem de produtos. Só é executada, porém,
+    se há elementos na lista 'produtos'."""
+
     if len(produtos) > 0:
         print('Listagem de produtos\n'
               '--------------------')
@@ -90,6 +103,11 @@ def listar_produtos() -> None:
 
 
 def comprar_produto() -> None:
+    """Função responsável pela compra de um produto incluído na lista
+    'produtos'. Só é executada se len(produtos) > 0. Uma vez executada,
+    solicita ao usuário o código do produto e, se for encontrado, este
+    será adicionado ao carrinho."""
+
     if len(produtos) > 0:
         print('====== Produtos Disponíveis ======')
 
@@ -98,8 +116,8 @@ def comprar_produto() -> None:
                   '----------------------------------')
             sleep(.5)
 
-        codigo: int = int(input('Informe o código do produto a ser ' + \
-            'adicionado ao carrinho: '))
+        codigo: int = int(input('Informe o código do produto a ser ' +
+                                'adicionado ao carrinho: '))
 
         produto: Produto = __pegar_prod_por_codigo(codigo)
 
@@ -116,6 +134,11 @@ def comprar_produto() -> None:
 
 
 def remover_produto() -> None:
+    """Função responsável pela remoção de um produto da lista 'produtos'.
+    Só é executada se len(produtos) > 0. Uma vez executada, solicita ao
+    usuário o código do produto e, se for encontrado, este será removido
+    do carrinho."""
+
     if len(produtos) > 0:
         print('====== Produtos Disponíveis ======')
 
@@ -124,8 +147,8 @@ def remover_produto() -> None:
                   '----------------------------------')
             sleep(.5)
 
-        codigo: int = int(input('Informe o código do produto a ser ' + \
-            'removido do carrinho: '))
+        codigo: int = int(input('Informe o código do produto a ser ' +
+                                'removido do carrinho: '))
 
         produto: Produto = __pegar_prod_por_codigo(codigo)
 
@@ -141,12 +164,18 @@ def remover_produto() -> None:
 
 
 def visualizar_carrinho() -> None:
+    """Chama o método visualizar() da classe Carrinho."""
+
     Carrinho.visualizar()
     sleep(1)
     menu()
 
 
 def fechar_pedido() -> None:
+    """Função responsável pelo fechamento do pedido através do método
+    .valor() da classe Carrinho. O valor da fatura será exibido no
+    formato R$ xxxx.xx e posteriormente o Carrinho será esvaziado."""
+
     valor = Carrinho.valor()
 
     if valor > 0:
@@ -161,6 +190,18 @@ def fechar_pedido() -> None:
 
 
 def __pegar_prod_por_codigo(codigo: int) -> Produto:
+    """Recebe um inteiro correspondendo ao código de um produto
+    e, caso o mesmo conste na lista 'produtos', será retornado.
+    Caso contrário, não há retorno.
+
+    Args:
+        codigo (int): código do produto a ser encontrado.
+
+    Returns:
+        Produto: produto da lista 'produtos' cujo código corresponda
+        ao código informado. Caso não haja correspondência: return None.
+    """
+
     for produto in produtos:
         if produto.codigo == codigo:
             return produto
